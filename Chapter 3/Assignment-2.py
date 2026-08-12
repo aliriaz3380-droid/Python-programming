6c6abf84455bcf6dde08f77549f8bee881bc723c
aliriaz3380-droid <aliriaz3380@gmail.com>
2026-08-12 12:24:06 +0500

    .

---
+++ Chapter 3/Assignment-2.py
@@ -0,0 +1,4 @@
+line=input("Enter the line:")
+print("The first character of line is:",line[0])
+print("The last charater of line is:",line[-1])
+print("The total number of characters in line is:",len(line))
