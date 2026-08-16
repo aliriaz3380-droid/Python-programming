2913b9db045d88a104c2f6602a044e091dca7c1d
aliriaz3380-droid <aliriaz3380@gmail.com>
2026-08-16 11:26:12 +0500

    Create Even-Odd Checker.py

---
+++ Multiple Projects.py/Even-Odd Checker.py
@@ -0,0 +1,9 @@
+number=float(input("Enter the number to find it,s Even or Odd:"))
+if number % 2==0:
+    print("The number is Even.")
+elif number % 2 !=0:
+    print("The number is Odd.") 
+elif number==0:
+    print("The number is Zero.") 
+else:
+    print("You have not put the number.")          
