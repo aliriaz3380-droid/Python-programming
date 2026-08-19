ba6986603fdae50b3473816d6c91b90323074aee
aliriaz3380-droid <aliriaz3380@gmail.com>
2026-08-19 13:41:32 +0500

    .

--- Multiple Projects.py/11-Shopping list.py
+++ Multiple Projects.py/11-Shopping list.py
@@ -4,38 +4,37 @@
 # 2. Remove item
 # 3. View items
 # 4. Exit
-
-print("Welcome to ABC Shopping Center")
-
-list1=[]
+print("Welcome to ABC Supper Store\n")
+item=[]
 while True:
-    print("1. Add item")
-    print("2. Remove item")
-    print("3. View items")
-    print("4. Exit")
-    choice=input("Enter your choice:")
-    if choice=="1":
-        n=int(input("How many items you want to add:"))
-        print(f"I want {n} items")
-        for item in range(n):
-            item=input("Enter the item Name:")
-            list1.append(item)
-        print(f"Your items are {list1}") 
-    elif choice=="2":
-            item=input("Enter the item name to remove:") 
-            if item in list1:
-              list1.remove(item)
-              print("item removed!")
-            else:
-                 print("item not found:")    
-    elif choice=="3" :
-            print(list1)  
+        print("1. Add items")
+        print("2. Remove items")
+        print("3. View items")
+        print("4. Exit")
+        choice=input("Enter the choice:")
+        if choice=="1":
+            while True:
+                i=input("Enter the item name(type done to finish):")
+                if i=="done":
+                    break
+                item.append(i)
+        elif choice=="2":
+             while True:
+                i=input("Enter the item name to remove or type end to end:")
+                if i=="end":
+                     break
+                item.remove(i) 
+                print("item is removed!")       
+        elif choice=="3":
+             print(item)  
+        elif choice=="4":
+             print("Exit")
+             item.clear()
+             print("Add the items again.")  
+        else:
+             print("invalid choice.")
+             break              
                 
-    elif choice=="4":
-            print("Exit")
-            list1.clear()
-            print("Add the items again.")
-    else:
-        print("invalid choice")    
-        break
-    
+                
+                
+                 
